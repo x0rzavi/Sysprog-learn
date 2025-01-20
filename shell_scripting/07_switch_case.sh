aeaaf56eb4d34c86b5aa1be05373f93c76366a42
Avishek Sen <x0rzavi@gmail.com>
2025-01-20 21:22:37 +0530

    minor fix

--- shell_scripting/07_switch_case.sh
+++ shell_scripting/07_switch_case.sh
@@ -3,9 +3,8 @@
 read -p "Enter name: " name
 
 case $name in
-"Avishek" | "avishek") echo -e "Hi Avi" ;;
-"Shruti" | "shruti") echo -e "Hi Shru" ;;
+"Alex" | "alex") echo -e "Hi A" ;;
+"Bob" | "bob") echo -e "Hi B" ;;
 esac
 
 echo "THE LENGTH OF THE STRING: ${#name}" #to find the length of the string use "#"
-
